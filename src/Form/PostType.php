ff16b367daea0f7fcfc7476354ad17157de8d602
Etienne Buffet <etienne.buffet@ac-grenoble.fr>
2023-09-22 15:26:30 +0200

    vich ok

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
